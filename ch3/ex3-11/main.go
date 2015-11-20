ac14d15dc592b85e4fdd7a9b8a83a7cfad97d946
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2015-11-20 07:39:55 -0700

    exercise 3.11

---
+++ ch3/ex3-11/main.go
@@ -0,0 +1,53 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 73.
+
+// Comma prints its argument numbers with a comma at each power of 1000.
+//
+// Example:
+// 	$ go build gopl.io/ch3/comma
+//	$ ./comma 1 12 123 1234 1234567890
+// 	1
+// 	12
+// 	123
+// 	1,234
+// 	1,234,567,890
+//
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	for i := 1; i < len(os.Args); i++ {
+		var sign rune
+		var fract string
+		s := os.Args[i]
+		if s[0] == '-' {
+			s = s[1:]
+			sign = '-'
+		}
+		if dot := strings.LastIndex(s, "."); dot > 0 {
+			fract = s[dot:]
+			s = s[:dot]
+		}
+
+		fmt.Printf("  %c%s%s\n", sign, comma(s), fract)
+	}
+}
+
+//!+
+// comma inserts commas in a non-negative decimal integer string.
+func comma(s string) string {
+	n := len(s)
+	if n <= 3 {
+		return s
+	}
+	return comma(s[:n-3]) + "," + s[n-3:]
+}
+
+//!-
