33eb47294129a2aba870b653519af8dbc68af83c
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2015-12-10 16:47:54 -0700

    Report results in age categories

---
+++ ch4/ex4-10/main.go
@@ -0,0 +1,99 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 112.
+//!+
+
+// Issues prints a table of GitHub issues matching the search terms.
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"gopl.io/ch4/github"
+)
+
+//!+
+func main() {
+	result, err := github.SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%d issues:\n", result.TotalCount)
+	var old, older, oldest []string
+	day := 24 * time.Hour
+	month := 30 * day
+	year := 365 * day
+
+	for _, item := range result.Items {
+		s := fmt.Sprintf("\t#%-5d %9.9s %.55s",
+			item.Number, item.User.Login, item.Title)
+		t := time.Since(item.CreatedAt)
+		switch {
+		case t < month:
+			old = append(old, s)
+		case t < year:
+			older = append(older, s)
+		default:
+			oldest = append(oldest, s)
+		}
+	}
+	fmt.Println("\nunder a month:\n")
+	for _, s := range old {
+		fmt.Println(s)
+	}
+	fmt.Println("\nunder a year:\n")
+	for _, s := range older {
+		fmt.Println(s)
+	}
+	fmt.Println("\nover a year:\n")
+	for _, s := range oldest {
+		fmt.Println(s)
+	}
+
+}
+
+//!-
+
+/*
+//!+textoutput
+$ go build gopl.io/ch4/issues
+$ ./issues repo:golang/go is:open json decoder
+13 issues:
+#5680    eaigner encoding/json: set key converter on en/decoder
+#6050  gopherbot encoding/json: provide tokenizer
+#8658  gopherbot encoding/json: use bufio
+#8462  kortschak encoding/json: UnmarshalText confuses json.Unmarshal
+#5901        rsc encoding/json: allow override type marshaling
+#9812  klauspost encoding/json: string tag not symmetric
+#7872  extempora encoding/json: Encoder internally buffers full output
+#9650    cespare encoding/json: Decoding gives errPhase when unmarshalin
+
+
+}
+
+//!-
+
+/*
+//!+textoutput
+$ go build gopl.io/ch4/issues
+$ ./issues repo:golang/go is:open json decoder
+13 issues:
+#5680    eaigner encoding/json: set key converter on en/decoder
+#6050  gopherbot encoding/json: provide tokenizer
+#8658  gopherbot encoding/json: use bufio
+#8462  kortschak encoding/json: UnmarshalText confuses json.Unmarshal
+#5901        rsc encoding/json: allow override type marshaling
+#9812  klauspost encoding/json: string tag not symmetric
+#7872  extempora encoding/json: Encoder internally buffers full output
+#9650    cespare encoding/json: Decoding gives errPhase when unmarshalin
+#6716  gopherbot encoding/json: include field name in unmarshal error me
+#6901  lukescott encoding/json, encoding/xml: option to treat unknown fi
+#6384    joeshaw encoding/json: encode precise floating point integers u
+#6647    btracey x/tools/cmd/godoc: display type kind of each named type
+#4237  gjemiller encoding/base64: URLEncoding padding is optional
+//!-textoutput
+*/
